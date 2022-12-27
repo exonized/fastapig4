d1272d70f87a35e56419bfdf22505b0327420864
Anthony Flori <aflori@wearehunty.fr>
2022-12-27 15:34:08 +0100

    fdgdfg

--- models.py
+++ models.py
@@ -1,22 +1,15 @@
 import sqlalchemy
+
 from database import metadata
-import passlib.hash as _hash
+
 
 usersbdd = sqlalchemy.Table(
     "users",
     metadata,
-    sqlalchemy.Column("id", sqlalchemy.INTEGER , primary_key=True, index=True),
-    sqlalchemy.Column("email", sqlalchemy.String, unique=True, index=True),
-    sqlalchemy.Column("pseudo", sqlalchemy.String, unique=True, index=True),
-    sqlalchemy.Column("roles", sqlalchemy.String, default=('Membre')),
-    sqlalchemy.Column("avatar", sqlalchemy.String , default=('Avatar/base.png')),
-    sqlalchemy.Column("hashed_password", sqlalchemy.String)
-
-    
-    
+    id = sqlalchemy.Column("id", sqlalchemy.INTEGER , primary_key=True, index=True),
+    email = sqlalchemy.Column("email", sqlalchemy.String, unique=True, index=True),
+    pseudo = sqlalchemy.Column("pseudo", sqlalchemy.String, unique=True, index=True),
+    roles = sqlalchemy.Column("roles", sqlalchemy.String, default=('Membre')),
+    avatar = sqlalchemy.Column("avatar", sqlalchemy.String , default=('Avatar/base.png')),
+    hashed_password = sqlalchemy.Column("hashed_password", sqlalchemy.String)
 )
-
-def verify_password(self, password: str):
-        return _hash.bcrypt.verify(password, self.hashed_password)
-
-
