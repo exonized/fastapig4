c8012a3459ae74d9e6863605a422da94a1e5813d
Anthony Flori <aflori@wearehunty.fr>
2022-12-27 15:01:09 +0100

    test bdd

--- models.py
+++ models.py
@@ -1,9 +1,19 @@
 import sqlalchemy
 from database import metadata
 
+usersbdd = sqlalchemy.Table(
+    "users",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.INTEGER , primary_key=True),
+    sqlalchemy.Column("users", sqlalchemy.String),
+    sqlalchemy.Column("mdp", sqlalchemy.String),
+)
+
+
 
 class User(sqlalchemy.Table):
-    __tablename__ = "users"
+    "users",
+    metadata,
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True)
     email = sqlalchemy.Column(sqlalchemy.String, unique=True, index=True)
     pseudo = sqlalchemy.Column(sqlalchemy.String, unique=True, index=True)
@@ -12,3 +22,5 @@ class User(sqlalchemy.Table):
     hashed_password = sqlalchemy.Column(sqlalchemy.String)
 
     
+    def verify_password(self, password: str):
+        return _hash.bcrypt.verify(password, self.hashed_password)
