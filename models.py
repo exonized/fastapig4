37209809b1b6e940e2713ab1da2454ed62aef17e
Anthony Flori <aflori@wearehunty.fr>
2022-12-27 14:57:31 +0100

    test bdd

--- models.py
+++ models.py
@@ -1,10 +1,14 @@
 import sqlalchemy
 from database import metadata
 
-usersbdd = sqlalchemy.Table(
-    "users",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.INTEGER , primary_key=True),
-    sqlalchemy.Column("users", sqlalchemy.String),
-    sqlalchemy.Column("mdp", sqlalchemy.String),
-)
+
+class User(sqlalchemy.Table):
+    __tablename__ = "users"
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True, index=True)
+    pseudo = sqlalchemy.Column(sqlalchemy.String, unique=True, index=True)
+    roles = sqlalchemy.Column(sqlalchemy.String,  default=('Membre'))
+    avatar = sqlalchemy.Column(sqlalchemy.String, default=('Avatar/base.png'))
+    hashed_password = sqlalchemy.Column(sqlalchemy.String)
+
+    
