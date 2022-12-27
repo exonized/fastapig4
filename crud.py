a6f6c0fa6a21de45b05748e6732c7d637142e460
Anthony Flori <aflori@wearehunty.fr>
2022-12-27 17:28:51 +0100

    test

--- crud.py
+++ crud.py
@@ -1,6 +1,5 @@
 import sqlalchemy.orm as _orm
 
-import jwt
 import passlib.hash as _hash
 
 import fastapi
@@ -10,84 +9,84 @@ import models
 import schemas
 import database
 
-oauth2schema = _security.OAuth2PasswordBearer(tokenUrl="/api/token")
+# oauth2schema = _security.OAuth2PasswordBearer(tokenUrl="/api/token")
 
-JWT_SECRET = "dfsdihfosdfjopisdjfiosdufoidshfiodsfhodsifhjdso"
-
-
-def get_db():
-    db = database.SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# JWT_SECRET = "dfsdihfosdfjopisdjfiosdufoidshfiodsfhodsifhjdso"
 
 
-async def get_user_by_email(email: str, db: _orm.Session):
-    return db.query(models.User).filter(models.User.email == email).first()
+# def get_db():
+#     db = database.SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 
 
-async def create_user(user: schemas.UserCreate, db: _orm.Session):
-    user_obj = models.User(
-        email=user.email, hashed_password=_hash.bcrypt.hash(user.hashed_password), pseudo=user.pseudo
-    )
-    db.add(user_obj)
-    db.commit()
-    db.refresh(user_obj)
-    return user_obj
+# async def get_user_by_email(email: str, db: _orm.Session):
+#     return db.query(models.User).filter(models.User.email == email).first()
 
 
-async def authenticate_user(email: str, password: str, db: _orm.Session):
-    user = await get_user_by_email(db=db, email=email)
-
-    if not user:
-        return False
-
-    if not user.verify_password(password):
-        return False
-
-    return user
+# async def create_user(user: schemas.UserCreate, db: _orm.Session):
+#     user_obj = models.User(
+#         email=user.email, hashed_password=_hash.bcrypt.hash(user.hashed_password), pseudo=user.pseudo
+#     )
+#     db.add(user_obj)
+#     db.commit()
+#     db.refresh(user_obj)
+#     return user_obj
 
 
-async def create_token(user: models.User):
-    user_obj = schemas.User.from_orm(user)
+# async def authenticate_user(email: str, password: str, db: _orm.Session):
+#     user = await get_user_by_email(db=db, email=email)
 
-    token = jwt.encode(user_obj.dict(), JWT_SECRET)
+#     if not user:
+#         return False
 
-    return dict(access_token=token, token_type="bearer", roles=user.roles, email=user.email)
+#     if not user.verify_password(password):
+#         return False
 
-
-async def get_current_user(
-    db: _orm.Session = fastapi.Depends(get_db),
-    token: str = fastapi.Depends(oauth2schema),
-):
-    try:
-        payload = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
-        user = db.query(models.User).get(payload["id"])
-    except:
-        raise fastapi.HTTPException(
-            status_code=401, detail="Mauvais email ou mot de passe"
-        )
-
-    return schemas.User.from_orm(user)
+#     return user
 
 
-async def delete_current_user(
-    db: _orm.Session = fastapi.Depends(get_db),
-    token: str = fastapi.Depends(oauth2schema),
-):
-    try:
-        payload = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
-        user = db.query(models.User).get(payload["id"])
-        db.delete(user)
-        db.commit()
+# async def create_token(user: models.User):
+#     user_obj = schemas.User.from_orm(user)
 
-    except:
-        raise fastapi.HTTPException(
-            status_code=401, detail="Problème lors de la supprésion"
-        )
+#     token = jwt.encode(user_obj.dict(), JWT_SECRET)
 
-    return schemas.User.from_orm(user)
+#     return dict(access_token=token, token_type="bearer", roles=user.roles, email=user.email)
+
+
+# async def get_current_user(
+#     db: _orm.Session = fastapi.Depends(get_db),
+#     token: str = fastapi.Depends(oauth2schema),
+# ):
+#     try:
+#         payload = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+#         user = db.query(models.User).get(payload["id"])
+#     except:
+#         raise fastapi.HTTPException(
+#             status_code=401, detail="Mauvais email ou mot de passe"
+#         )
+
+#     return schemas.User.from_orm(user)
+
+
+# async def delete_current_user(
+#     db: _orm.Session = fastapi.Depends(get_db),
+#     token: str = fastapi.Depends(oauth2schema),
+# ):
+#     try:
+#         payload = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+#         user = db.query(models.User).get(payload["id"])
+#         db.delete(user)
+#         db.commit()
+
+#     except:
+#         raise fastapi.HTTPException(
+#             status_code=401, detail="Problème lors de la supprésion"
+#         )
+
+# return schemas.User.from_orm(user)
 
 
 def get_items(db:  _orm.Session, skip: int = 0, limit: int = 100):
