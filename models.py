d71c6dfa3c35aa7a0a4ee8e53a9f53f0822b2d9b
Anthony Flori <aflori@wearehunty.fr>
2022-12-27 15:15:07 +0100

    tfydj

--- models.py
+++ models.py
@@ -1,13 +1,13 @@
 import sqlalchemy
+import database as _database
+from database import metadata
 
-
-Utilisateurs = sqlalchemy.Table(
+usersbdd = sqlalchemy.Table(
     "users",
-    sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True),
-    sqlalchemy.Column(sqlalchemy.String, unique=True, index=True),
-    sqlalchemy.Column(sqlalchemy.String, unique=True, index=True),
-    sqlalchemy.Column(sqlalchemy.String,  default=('Membre')),
-    sqlalchemy.Column(sqlalchemy.String, default=('Avatar/base.png'))
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.INTEGER , primary_key=True),
+    sqlalchemy.Column("users", sqlalchemy.String),
+    sqlalchemy.Column("mdp", sqlalchemy.String),
 )
 
 
