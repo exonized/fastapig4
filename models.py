064c45dffdb800131000f550dfafb8e1816e27c9
Anthony Flori <aflori@wearehunty.fr>
2022-12-27 15:10:38 +0100

    jbj

--- models.py
+++ models.py
@@ -5,11 +5,11 @@ from database import metadata
 Utilisateurs = sqlalchemy.Table(
     "users",
     metadata,
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True),
-    email = sqlalchemy.Column(sqlalchemy.String, unique=True, index=True),
-    pseudo = sqlalchemy.Column(sqlalchemy.String, unique=True, index=True),
-    roles = sqlalchemy.Column(sqlalchemy.String,  default=('Membre')),
-    avatar = sqlalchemy.Column(sqlalchemy.String, default=('Avatar/base.png'))
+    sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True),
+    sqlalchemy.Column(sqlalchemy.String, unique=True, index=True),
+    sqlalchemy.Column(sqlalchemy.String, unique=True, index=True),
+    sqlalchemy.Column(sqlalchemy.String,  default=('Membre')),
+    sqlalchemy.Column(sqlalchemy.String, default=('Avatar/base.png'))
 )
 
 
