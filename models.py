64fa8c750f0946f3ab6cf6647b503bcc20239793
Anthony Flori <aflori@wearehunty.fr>
2022-12-27 15:21:23 +0100

    sdfgjxiodfs

--- models.py
+++ models.py
@@ -1,17 +1,14 @@
 import sqlalchemy
-import sqlalchemy as _sql
-import database as _database
 from database import metadata
 
 usersbdd = sqlalchemy.Table(
     "users",
     metadata,
-    id = sqlalchemy.Column(_sql.Integer, primary_key=True, index=True),
-    email = sqlalchemy.Column(_sql.String, unique=True, index=True),
-    pseudo = sqlalchemy.Column(_sql.String, unique=True, index=True),
-    roles = sqlalchemy.Column(_sql.String,  default=('Membre')),
-    avatar = sqlalchemy.Column(_sql.String, default=('Avatar/base.png'))
-
+    sqlalchemy.Column("id", sqlalchemy.INTEGER , primary_key=True, index=True),
+    sqlalchemy.Column("email", sqlalchemy.String, unique=True, index=True),
+    sqlalchemy.Column("pseudo", sqlalchemy.String, unique=True, index=True),
+    sqlalchemy.Column("roles", sqlalchemy.String, default=('Membre')),
+    sqlalchemy.Column("avatar", sqlalchemy.String , default=('Avatar/base.png'))
     
 )
 
