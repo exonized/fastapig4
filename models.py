4a24a4e94230b9e9eec0004de822584051284659
Anthony Flori <aflori@wearehunty.fr>
2022-12-27 15:08:39 +0100

    models users

--- models.py
+++ models.py
@@ -2,14 +2,14 @@ import sqlalchemy
 import database as _database
 from database import metadata
 
-
-class User(_database.Base):
+Utilisateurs = sqlalchemy.Table(
     "users",
     metadata,
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True)
-    email = sqlalchemy.Column(sqlalchemy.String, unique=True, index=True)
-    pseudo = sqlalchemy.Column(sqlalchemy.String, unique=True, index=True)
-    roles = sqlalchemy.Column(sqlalchemy.String,  default=('Membre'))
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True),
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True, index=True),
+    pseudo = sqlalchemy.Column(sqlalchemy.String, unique=True, index=True),
+    roles = sqlalchemy.Column(sqlalchemy.String,  default=('Membre')),
     avatar = sqlalchemy.Column(sqlalchemy.String, default=('Avatar/base.png'))
-    hashed_password = sqlalchemy.Column(sqlalchemy.String)
+)
+
 
