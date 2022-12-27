4b0f6f28893a529ee6b8c31730a1c6c84ac93fed
Anthony Flori <aflori@wearehunty.fr>
2022-12-27 15:18:18 +0100

    wsg

--- models.py
+++ models.py
@@ -1,13 +1,18 @@
 import sqlalchemy
+import sqlalchemy as _sql
 import database as _database
 from database import metadata
 
 usersbdd = sqlalchemy.Table(
     "users",
     metadata,
-    sqlalchemy.Column("id", sqlalchemy.INTEGER , primary_key=True),
-    sqlalchemy.Column("users", sqlalchemy.String),
-    sqlalchemy.Column("mdp", sqlalchemy.String),
+    id = sqlalchemy.Column(_sql.Integer, primary_key=True, index=True),
+    email = sqlalchemy.Column(_sql.String, unique=True, index=True),
+    pseudo = sqlalchemy.Column(_sql.String, unique=True, index=True),
+    roles = sqlalchemy.Column(_sql.String,  default=('Membre')),
+    avatar = sqlalchemy.Column(_sql.String, default=('Avatar/base.png'))
+
+    
 )
 
 
