e56241d4810108a60de23de59f60946d5ba051b6
Anthony Flori <aflori@wearehunty.fr>
2022-12-29 10:09:46 +0100

    cc

--- crud.py
+++ crud.py
@@ -16,3 +16,39 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def get_items(db:  _orm.Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Item).offset(skip).limit(limit).all()
+
+
+def create_user_item(item: schemas.ItemCreate, db:  _orm.Session):
+    db_item = models.Item(**item.dict())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+
+def contact_create(contact: schemas.ContactCreate, db: _orm.Session):
+    db_contact = models.Contact(**contact.dict())
+    db.add(db_contact)
+    db.commit()
+    db.refresh(db_contact)
+    return db_contact
+
+
+def get_contact(db:  _orm.Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Contact).offset(skip).limit(limit).all()
+
+
+def get_services(db:  _orm.Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Services).offset(skip).limit(limit).all()
+
+
+def create_services(services: schemas.ServiceCreate, db:  _orm.Session):
+    db_services = models.Services(**services.dict())
+    db.add(db_services)
+    db.commit()
+    db.refresh(db_services)
+    return db_services
--- main.py
+++ main.py
@@ -26,3 +26,56 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+# API items (articles)
+
+
+@app.post("/api/items/", tags=["items"], response_model=schemas.Item)
+def create_item_for_user(
+    item: schemas.ItemCreate, db: _orm.Session = fastapi.Depends(crud.get_db)
+):
+    return crud.create_user_item(db=db, item=item)
+
+
+@app.get("/api/items/get", tags=["items"], response_model=List[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: _orm.Session = fastapi.Depends(crud.get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
+
+
+# API contact
+
+
+@app.post("/api/contact/", tags=["contact"], response_model=schemas.Contact)
+def create_contact_for_user(
+    contact: schemas.ContactCreate, db: _orm.Session = fastapi.Depends(crud.get_db)
+):
+    return crud.contact_create(db=db, contact=contact)
+
+
+@app.get("/api/contact/get", tags=["contact"], response_model=List[schemas.Contact])
+def read_contact(skip: int = 0, limit: int = 10, db: _orm.Session = fastapi.Depends(crud.get_db)):
+    contact = crud.get_contact(db, skip=skip, limit=limit)
+    return contact
+
+
+# API services (services)
+
+
+@app.post("/api/services/", tags=["services"], response_model=schemas.Service)
+def create_services(
+    services: schemas.ServiceCreate, db: _orm.Session = fastapi.Depends(crud.get_db)
+):
+    return crud.create_services(db=db, services=services)
+
+
+@app.get("/api/services/get", tags=["services"], response_model=List[schemas.Service])
+def read_services(skip: int = 0, limit: int = 10, db: _orm.Session = fastapi.Depends(crud.get_db)):
+    services = crud.get_services(db, skip=skip, limit=limit)
+    return services
+
+
+@app.get("/", tags=["api"])
+def read_services():
+    return "API ANTHONY"
