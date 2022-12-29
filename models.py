71df960aaa91b4419dd0985799b65ea4fc7ef130
Anthony Flori <aflori@wearehunty.fr>
2022-12-29 10:52:32 +0100

    cccc

--- models.py
+++ models.py
@@ -5,20 +5,25 @@ from database import Base
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "Utilisateurs"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     pseudo = Column(String, unique=True, index=True)
     roles = Column(String,  default=('Membre'))
     avatar = Column(String, default=('Avatar/base.png'))
     hashed_password = Column(String)
+    adresse = Column(String)
+    completement = Column(String)
+    codepostal = Column(Integer)
+    region = Column(String)
+    numerorue = Column(Integer)
 
     def verify_password(self, password: str):
         return _hash.bcrypt.verify(password, self.hashed_password)
 
 
 class Item(Base):
-    __tablename__ = "items"
+    __tablename__ = "Produits"
 
     id = Column(Integer, primary_key=True, index=True)
     titre = Column(String, index=True)
@@ -27,20 +32,69 @@ class Item(Base):
     image = Column(String, index=True)
 
 
-class Contact(Base):
-    __tablename__ = "contact"
+class Devis(Base):
+    __tablename__ = "Devis"
     id = Column(Integer, primary_key=True, index=True)
-    titre = Column(String, index=True)
-    description = Column(String, index=True)
+    user_id = Column(Integer)
+    description = Column(String)
+    pdf = Column(String)
+
+
+class Facture(Base):
+    __tablename__ = "Facture"
+    id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, index=True)
-    image = Column(String, index=True)
+    description = Column(String)
+    pdf = Column(String)
+
+
+class Feedback(Base):
+    __tablename__ = "Feedback"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer)
+    ratting_feedback = Column(Integer)
+    titre_feedback = Column(String)
+    contenu_feedback = Column(String)
+
+
+class Contact(Base):
+    __tablename__ = "Contact"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer)
+    objet = Column(String, index=True)
+    message = Column(String, index=True)
 
 
 class Services(Base):
-    __tablename__ = "services"
+    __tablename__ = "Services"
 
     id = Column(Integer, primary_key=True, index=True)
     titre = Column(String, index=True)
     descripion = Column(String, index=True)
     image = Column(String, index=True)
     prix = Column(Integer, index=True)
+
+
+class Chatbox(Base):
+    __tablename__ = "Chatbox"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer)
+    pseudo = Column(String)
+    message = Column(String)
+
+
+class Commandes(Base):
+    __tablename__ = "Commandes"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer)
+    articles_id = Column(Integer)
+
+
+class Articles(Base):
+    __tablename__ = "Articles"
+    id = Column(Integer, primary_key=True, index=True)
+    types = Column(String)
+    titre = Column(String)
+    description = Column(String)
+    contenu = Column(String)
+    images = Column(String)
