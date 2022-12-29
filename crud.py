9ab6eaec2885909c08f9065ea204b4c2af1c6e0e
Anthony Flori <aflori@wearehunty.fr>
2022-12-29 11:40:48 +0100

    koi

--- crud.py
+++ crud.py
@@ -93,14 +93,11 @@ def get_items(db:  _orm.Session, skip: int = 0, limit: int = 100):
     return db.query(models.Item).offset(skip).limit(limit).all()
 
 
-def create_user_item(item: schemas.ItemCreate, db:  _orm.Session,  token: str = fastapi.Depends(oauth2schema)):
-    payload = jwtt.decode(token, JWT_SECRET, algorithms=["HS256"])
-    user = db.query(models.User).get(payload["id"])
-    if user:
-        db_item = models.Item(**item.dict())
-        db.add(db_item)
-        db.commit()
-        db.refresh(db_item)
+def create_user_item(item: schemas.ItemCreate, db:  _orm.Session):
+    db_item = models.Item(**item.dict())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
     return db_item
 
 
